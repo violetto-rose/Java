bcc2d7c54782d7542846c9403fda7d7bb97ed85a
violetto-rose <manjumadhav.va@gmail.com>
2024-10-22 11:30:46 +0530

    renamed:    Package10.java -> Lab Programs/10Package.java 	renamed:    Threads11.java -> Lab Programs/11Threads.java 	renamed:    MyThread12.java -> Lab Programs/12MyThread.java 	renamed:    Matrix1.java -> Lab Programs/1Matrix.java 	renamed:    IntegerStack2.java -> Lab Programs/2IntegerStack.java 	renamed:    Employee3.java -> Lab Programs/3Employee.java 	renamed:    MyPoint4.java -> Lab Programs/4MyPoint.java 	renamed:    TestMyPoint4.java -> Lab Programs/4TestMyPoint.java 	renamed:    ShapeDemo5.java -> Lab Programs/5ShapeDemo.java 	renamed:    ShapeCalculator6.java -> Lab Programs/6ShapeCalculator.java 	renamed:    Resize7.java -> Lab Programs/7Resize.java 	renamed:    OutIn8.java -> Lab Programs/8OutIn.java 	renamed:    Exception9.java -> Lab Programs/9Exception.java 	deleted:    Main.java 	renamed:    Multi.java -> Multiplication.java 	modified:   Swap.java

--- Multiplication.java
+++ Multiplication.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Multi {
+class Multiplication {
     public static void main(String[] args) {
         int a, b, product;
         Scanner sc = new Scanner(System.in);
